5048bb33057d141550d60d1d997e9522dd9edb20
n8goody <n8goody@gmail.com>
2026-06-19 10:40:10 -0500

    add cjezz logo and mod deploy.sh for dev-nate to exclude ver num

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Grab the first word you type after the script name
-VERSION=$1
+#VERSION=$1
 
 # Stop the script if you forgot to type a version
-if [ -z "$VERSION" ]; then
-  echo "Error: Please provide a version tag."
-  echo "Usage: ./deploy.sh v2.0.0"
-  exit 1
-fi
+# if [ -z "$VERSION" ]; then
+#   echo "Error: Please provide a version tag."
+#   echo "Usage: ./deploy.sh v2.0.0"
+#   exit 1
+# fi
 
-echo "Building and pushing version: $VERSION..."
+#echo "Building and pushing version: $VERSION..."
 
 # Build with both tags
-docker build -t n8goody/chessviewer:$VERSION -t n8goody/chessviewer:dev-nate .
+docker build  -t n8goody/chessviewer:dev-nate . #-t n8goody/chessviewer:$VERSION
 
 # Push both tags
-docker push n8goody/chessviewer:$VERSION
+#docker push n8goody/chessviewer:$VERSION
 docker push n8goody/chessviewer:dev-nate
 
-echo "Deployment Complete for $VERSION!"
+#echo "Deployment Complete for $VERSION!"
